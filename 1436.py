dd736ba9367bd91151ef8a26f88062bf44407dca
Sieun Choi <qdial214@dgu.ac.kr>
2022-11-15 22:55:13 +0900

    Update 1436.py
    N이 10000 이하의 값이라는 제한 조건을 이용하여 코드를 수정하였다.

--- 1436.py
+++ 1436.py
@@ -1,8 +1,10 @@
 N = int(input())
-name_list=[]
-for i in range(2666800):
+name_list = []
+i = 0
+while len(name_list) <= 10000:
+    i += 1
     if '666' in str(i):
         name_list.append(i)
-        if len(name_list)==N:
+        if len(name_list) == N:
             print(name_list[N - 1])
             break
