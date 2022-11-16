84a80bf988cebefa8c28d5d941cd4ed1fa6f3fd1
Sieun Choi <qdial214@dgu.ac.kr>
2022-11-17 04:37:20 +0900

    Update 10814.py
    시간 초과 해결
    - sort 대신 sorted 연산 사용
    - 리스트 조회 시 인덱스 이용하지 않고 요소에 직접 접근

--- 10814.py
+++ 10814.py
@@ -4,7 +4,5 @@ for i in range(N):
     member.append(list(input().split()))
     member[i][0] = int(member[i][0])
     member[i].insert(0, i)
-    member.sort(key=lambda x: (x[1], x[0]))
-
-for i in range(N):
-    print(member[i][1], member[i][2])
+for mem in sorted(member,key=lambda x: (x[1], x[0])):
+    print(mem[1],mem[2])
