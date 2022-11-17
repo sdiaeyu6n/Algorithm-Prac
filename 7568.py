055c5d30fc03b225ab61cb61d738d7eb201f0917
Sieun Choi <qdial214@dgu.ac.kr>
2022-11-17 18:09:55 +0900

    Update 7568.py
    해결
    - 진원오빠 도움 받음.
    - 이중 for문 사용 주의
    - 비교 주의

--- 7568.py
+++ 7568.py
@@ -2,17 +2,12 @@ N = int(input())
 people_list = []
 
 for i in range(N):
-    people_list.append(list(map(int, input().split())))
-smaller = [0 for i in range(N)]
+    people_list.append(list(map(int, input().split())))    # [140,60]  // [140,60]2
+rank = [1 for i in range(N)]
 for j in range(N):
     for k in range(N):
-        if min((people_list[j], people_list[k]), key=lambda x: x[1]) == people_list[j] \
-                and min((people_list[j], people_list[k])) == people_list[j]:
-            smaller[j] += 1
-
-rank=[]
-for i in range(N):
-    rank.append(smaller[i])
+        if people_list[j][0]<people_list[k][0] and people_list[j][1]<people_list[k][1]:
+            rank[j]+=1
 
 for i in rank:
     print(i, end=' ')
