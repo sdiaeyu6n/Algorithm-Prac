5f5cc9e4e49820fc563911a05ff960fd96ad64f9
Sieun Choi <qdial214@dgu.ac.kr>
2023-01-16 04:04:08 +0900

    Update 1018.py
    정답은 2개뿐이다. 정답 그래프와 일치하는지만 확인하면 된다!
    -> 로직 이해했고 코드로 구현했으나 답이 틀리는 상황

--- 1018.py
+++ 1018.py
@@ -1,10 +1,10 @@
 import sys
+
 N,M=map(int,sys.stdin.readline().split())
 board=[['0']*M for i in range(N)]
 new_board=[['0']*8 for i in range(8)]
 for i in range(N):
     board[i]=list(sys.stdin.readline().strip("\n"))
-
 
 board_list=[]
 
@@ -15,5 +15,34 @@ for i in range(N-8+1):
                 new_board[a][b]=board[i+a][j+b]
         board_list.append(new_board)
 
+graph1=['BWBWBWBW',
+'WBWBWBWB',
+'BWBWBWBW',
+'WBWBWBWB',
+'BWBWBWBW',
+'WBWBWBWB',
+'BWBWBWBW',
+'WBWBWBWB']
 
+graph2=['WBWBWBWB',
+'BWBWBWBW',
+'WBWBWBWB',
+'BWBWBWBW',
+'WBWBWBWB',
+'BWBWBWBW',
+'WBWBWBWB',
+'BWBWBWBW']
+
+temp=9999
 for b in board_list:
+    count1=0
+    count2=0
+    for i in range(8):
+        for j in range(8):
+            if b[i][j]!=graph1[i][j]:
+                count1+=1
+            if b[i][j]!=graph2[i][j]:
+                count2+=1
+    temp=min(temp,count1,count2)
+
+print(temp)
