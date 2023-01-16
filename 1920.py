51c788e88ddd250088ba89d113ad0f9d62123a33
Sieun Choi <qdial214@dgu.ac.kr>
2023-01-16 14:23:42 +0900

    Delete 1920.py

--- 1920.py
+++
@@ -1,9 +0,0 @@
-N=int(input())
-N_list=list(map(int,input().split()))
-M=int(input())
-M_list=list(map(int,input().split()))
-
-for m in M_list:
-    if m in N_list:
-        print(1)
-    else: print(0)
