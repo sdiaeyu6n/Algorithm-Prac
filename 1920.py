953b1df132cb35353afa5bc4f4eabdba4bd4f671
Sieun Choi <qdial214@dgu.ac.kr>
2023-01-16 17:42:47 +0900

    Refactoring - Function
    함수는 제일 위에 정의하기

--- 1920.py
+++ 1920.py
@@ -1,12 +1,6 @@
 # 시간복잡도 = O((M+N)logN)
 import sys
 
-N=int(sys.stdin.readline())
-# Tim Sort -> O(NlogN)
-N_list=sorted(list(map(int,sys.stdin.readline().split())))
-M=int(sys.stdin.readline())
-M_list=list(map(int,sys.stdin.readline().split()))
-
 # 이분탐색 M번 수행 -> O(MlogN)
 def binary_search(target,array,start,end):
     if start>end:
@@ -20,6 +14,12 @@ def binary_search(target,array,start,end):
         return binary_search(target,array,mid+1,end)
     else:
         return binary_search(target,array,start,mid-1)
+
+N=int(sys.stdin.readline())
+# Tim Sort -> O(NlogN)
+N_list=sorted(list(map(int,sys.stdin.readline().split())))
+M=int(sys.stdin.readline())
+M_list=list(map(int,sys.stdin.readline().split()))
 
 for m in M_list:
     binary_search(m,N_list,0,N-1) #범위 유의(end=N-1)
