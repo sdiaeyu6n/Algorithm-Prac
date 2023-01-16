3e2fe33b1c8dc9b67b0432d0f5ed14d2cf583128
Sieun Choi <qdial214@dgu.ac.kr>
2023-01-16 17:35:57 +0900

    수 찾기(이분탐색)
    - 시간복잡도를 계산해보고, 적합한 알고리즘을 찾아서 적용!

--- 1920.py
+++ 1920.py
@@ -1,9 +1,25 @@
-N=int(input())
-N_list=list(map(int,input().split()))
-M=int(input())
-M_list=list(map(int,input().split()))
+# 시간복잡도 = O((M+N)logN)
+import sys
+
+N=int(sys.stdin.readline())
+# Tim Sort -> O(NlogN)
+N_list=sorted(list(map(int,sys.stdin.readline().split())))
+M=int(sys.stdin.readline())
+M_list=list(map(int,sys.stdin.readline().split()))
+
+# 이분탐색 M번 수행 -> O(MlogN)
+def binary_search(target,array,start,end):
+    if start>end:
+        print(0)
+        return 0
+    mid = (start+end)//2
+    if array[mid]==target:
+        print(1)
+        return 1
+    elif array[mid]<target:
+        return binary_search(target,array,mid+1,end)
+    else:
+        return binary_search(target,array,start,mid-1)
 
 for m in M_list:
-    if m in N_list:
-        print(1)
-    else: print(0)
+    binary_search(m,N_list,0,N-1) #범위 유의(end=N-1)
