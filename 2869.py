bd7f67658f6da7596df8616def59ed0644e61155
Sieun Choi <qdial214@dgu.ac.kr>
2023-01-16 22:57:09 +0900

    Update 2869.py
    시간초과

--- 2869.py
+++ 2869.py
@@ -1,18 +1,10 @@
 import sys
 
 A,B,V=map(int,sys.stdin.readline().split())
+count=1
+d=A
+while d<V:
+    d=d-B+A
+    count+=1
 
-d1=0
-d2=0
-eq1=A*d1-B*d1-V # 답=d
-eq2=A*(d2+1)-B*d2-V # 답=d+1
-
-while eq1<0:
-    d1+=1
-    eq1=A*d1-B*d1-V # 답=d
-
-while eq2<0:
-    d2+=1
-    eq2=A*(d2+1)-B*d2-V # 답=d+1
-    
-print(min(d1,d2+1))
+print(count)
