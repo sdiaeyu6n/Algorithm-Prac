4c7340b0f8ac4ef93ef361bed240acd562cd4b28
Sieun Choi <qdial214@dgu.ac.kr>
2023-01-19 18:23:48 +0900

    Update 1929.py
    - 소수의 제곱 형태 -> 소수가 아닌 수로 분류해야 함.

--- 1929.py
+++ 1929.py
@@ -2,11 +2,16 @@ import sys
 
 M,N=map(int,sys.stdin.readline().split())
 num_list=[0]*(N-M+1)
-for i in range(M,N):
-    for j in range(2,i):
-        if i%j==0:
-            num_list[i-M]+=1
 
-for i in range(M,N):
+for i in range(M,N+1):
+    if i==1:
+        num_list[i-M]+=1
+    elif i==2 or i==3 or i==5 or i==7:
+        continue
+    else:
+        if i%2==0 or i%3==0 or i%5==0 or i%7==0:
+            num_list[i-M]+=1  
+         
+for i in range(M,N+1):
     if num_list[i-M]==0:
         print(i)
