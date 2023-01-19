35f7b5ea918bc5b91400c74acb3b37826eca0d2b
Sieun Choi <qdial214@dgu.ac.kr>
2023-01-19 18:35:21 +0900

    Update 1929.py
    소수의 제곱 형태는 걸러내지만, 소수x소수 형태의 수는 걸러내지못함.

--- 1929.py
+++ 1929.py
@@ -1,4 +1,4 @@
-import sys
+import sys, math
 
 M,N=map(int,sys.stdin.readline().split())
 num_list=[0]*(N-M+1)
@@ -9,7 +9,8 @@ for i in range(M,N+1):
     elif i==2 or i==3 or i==5 or i==7:
         continue
     else:
-        if i%2==0 or i%3==0 or i%5==0 or i%7==0:
+        test=(int(math.sqrt(i)) == math.sqrt(i))
+        if i%2==0 or i%3==0 or i%5==0 or i%7==0 or test:
             num_list[i-M]+=1  
          
 for i in range(M,N+1):
