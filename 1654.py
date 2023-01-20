7980edc6ddd437201a7ffb9a65900585eb8f9266
Sieun Choi <qdial214@dgu.ac.kr>
2023-01-21 00:45:12 +0900

    Update 1654.py
    시간초과 -> 리스트의 인덱스로 수를 표현해서 해결해보자

--- 1654.py
+++ 1654.py
@@ -4,3 +4,12 @@ K,N=map(int, sys.stdin.readline().split())
 have=[0]*K
 for i in range(K):
     have[i]=int(sys.stdin.readline())
+
+temp_length=0
+for i in range(1,max(have)):
+    count=0
+    for n in have:
+        count+=n//i
+    if count>=N:
+        temp_length=max(temp_length,i)
+print(temp_length)
