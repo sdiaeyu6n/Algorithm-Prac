151d1d15199426da006d0c0abbb03bb05fd6fafa
Sieun Choi <qdial214@dgu.ac.kr>
2023-01-21 02:32:46 +0900

    랜선 자르기 / 이분탐색 / ★
    - 정답 코드 보고 해결함
    - 입력 범위가 매우 크기 때문에 이분탐색을 적용해볼 필요가 있다.
    - 각 변수와, 각 조건 등이 의미하는 것이 무엇인지 정확하게 인지하고 코드를 작성해야 한다.

--- 1654.py
+++ 1654.py
@@ -1,15 +1,20 @@
 import sys
 
+def binary_search(have,start,end):
+    if start>end:
+        return end
+    mid=(start+end)//2
+    count=0
+    for n in have:
+        count+=n//mid
+    if count>=N:
+        return binary_search(have,mid+1,end)
+    else: #count<11
+        return binary_search(have,start,mid-1)
+
 K,N=map(int, sys.stdin.readline().split())
 have=[0]*K
 for i in range(K):
     have[i]=int(sys.stdin.readline())
 
-temp_length=0
-for i in range(1,max(have)):
-    count=0
-    for n in have:
-        count+=n//i
-    if count>=N:
-        temp_length=max(temp_length,i)
-print(temp_length)
+print(binary_search(have,1,max(have)))
