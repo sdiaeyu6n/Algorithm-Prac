98e24bc7d177aecb24136a38f5b5c9d23c9aaf1d
Sieun Choi <qdial214@dgu.ac.kr>
2023-01-27 02:56:07 +0900

    연결 요소의 개수 / dfs
    런타임 에러 (RecursionError)

---
+++ 11724.py
@@ -0,0 +1,24 @@
+def dfs(start,graph,visited):
+    visited[start]=True
+    for i in graph[start]:
+        if not visited[i]:
+            dfs(i,graph,visited)
+
+N,M=map(int, input().split())
+graph=[[] for _ in range(N+1)]
+
+for i in range(M):
+    n1,n2=map(int,input().split())
+    graph[n1].append(n2)
+    graph[n2].append(n1)
+
+visited=[False]*(N+1)
+
+num=0    
+for i in range(N):
+    for j in graph[i]:
+        if not visited[j]:
+            dfs(j,graph,visited)
+            num+=1
+
+print(num)
