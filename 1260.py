432b56845ca01d36abfea6f632f9fb6e8a93ed7b
Sieun Choi <qdial214@dgu.ac.kr>
2023-01-26 19:33:03 +0900

    Refactoring #1260

--- 1260.py
+++ 1260.py
@@ -1,19 +1,5 @@
 from collections import deque
 
-N,M,V=map(int, input().split())
-graph = [[] for _ in range(N+1)]
-
-for i in range(M):
-    n1,n2=map(int,input().split())
-    graph[n1].append(n2)
-    graph[n2].append(n1) # 간선은 양방향이다.
-
-# 조건(숫자가 작은 노드부터)
-for i in range(N+1):
-    graph[i].sort()
-
-visited=[False]*(N+1)
-
 def DFS(start,graph,visited):
     visited[start]=True
     print(start,end=" ")
@@ -32,6 +18,20 @@ def BFS(V,graph,visited):
             if not visited[i]:
                 queue.append(i)
                 visited[i]=True
+
+N,M,V=map(int, input().split())
+graph = [[] for _ in range(N+1)]
+
+for i in range(M):
+    n1,n2=map(int,input().split())
+    graph[n1].append(n2)
+    graph[n2].append(n1) # 간선은 양방향이다.
+
+# 조건(숫자가 작은 노드부터)
+for i in range(N+1):
+    graph[i].sort()
+
+visited=[False]*(N+1)
 
 DFS(V,graph,visited)
 visited=[False]*(N+1)
